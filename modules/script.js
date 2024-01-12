f94fdd6012f6797ae118154857c8a93f4cd6824d
Rory Wood <rorywood9@live.com>
2024-01-12 01:22:34 +0000

    updated script

--- modules/script.js
+++ modules/script.js
@@ -1,10 +1,10 @@
 console.log("script.js Linked");
 
+// Sticky navbar
 var navBar = document.getElementById("navbar");
 var navDim = navBar.offsetTop;
 
 window.onscroll = function () {
-  // Sticky navbar
   if (window.pageYOffset >= navDim) {
     navBar.classList.add("sticky");
   } else {
@@ -12,6 +12,7 @@ window.onscroll = function () {
   }
 };
 
+// Navbar opening on-click
 var menuButton = document.getElementById("menu_icon");
 var navButtons = navBar.getElementsByClassName("button");
 var navHidden = true;
@@ -22,16 +23,12 @@ menuButton.onclick = function () {
     navHidden = false;
     console.log(navButtons.length);
     for (let i = 0; i < navButtons.length; i++) {
-      //navButtons[i].style.display = 'flex';
-      //navButtons[i].style.width = '150px';
       navButtons[i].style.left = "0px";
     }
   } else {
     navBar.style.width = "50px";
     navHidden = true;
     for (let i = 0; i < navButtons.length; i++) {
-      //navButtons[i].style.display = 'none';
-      //navButtons[i].style.width = '0px';
       navButtons[i].style.left = "-200px";
     }
   }
